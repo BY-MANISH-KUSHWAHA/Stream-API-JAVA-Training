3344de276cfde616404c9f5ca7278b094a849d0a
BY-Manish-Kushwaha <manish.kushwaha@blueyonder.com>
2022-09-08 10:19:55 +0530

    Stream_API-Update-0003

--- src/com/introduction/Company.java
+++ src/com/introduction/Company.java
@@ -5,4 +5,8 @@ public interface Company {
     default void updateCompany(){
         System.out.println("Call from Interface: Updating the company Details (without extends we have a parent method)");
     }
+
+    static  void deleteCompany(){
+        System.out.println("I'm deleting the company details.");
+    }
 }
--- src/com/introduction/Methods.java
+++ src/com/introduction/Methods.java
@@ -8,11 +8,25 @@ public class Methods implements Company{
     public void insertCompany() {
         System.out.println("Inserting a Company.");
     }
+    @Override // can be override or used interface one
+    public void updateCompany(){
+        System.out.println("Call from implemented class");
+    }
 
     public static void main(String[] args) {
         Methods obj = new Methods();
         obj.insertCompany();
         obj.updateCompany();
+
+        Methods obj_ = new Methods();
+        obj_.insertCompany();
+        obj_.updateCompany();
+
+        // Call the static method
+        Company.deleteCompany();
+
+
+
     }
 }
 
