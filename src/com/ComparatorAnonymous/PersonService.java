6b8516f685ce1c25ebf2a40973c5b673c23d1e36
BY-Manish-Kushwaha <manish.kushwaha@blueyonder.com>
2022-09-08 14:26:41 +0530

    Stream_API-Update-0010

--- src/com/ComparatorAnonymous/PersonService.java
+++ src/com/ComparatorAnonymous/PersonService.java
@@ -2,13 +2,17 @@ package com.ComparatorAnonymous;
 import java.util.*;
 public class PersonService {
     public static List<Person> getPerson(List<Person> persons){
-        Collections.sort(persons,new Comparator<Person>(){
+        // Anonymous Class Approach, Comparator is anonymous class
+/*        Collections.sort(persons,new Comparator<Person>(){
             @Override
             public int compare(Person o1, Person o2) {
                 return o1.getName().compareTo(o2.getName());
             }
         });
 
+ */
+        // Using Labda Expresson
+        Collections.sort(persons, (o1,o2) -> o1.getName().compareTo(o2.getName()));
         return persons;
     }
 }
